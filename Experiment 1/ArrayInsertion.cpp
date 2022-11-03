bac7ecc8cd04d9854fa2247be7747e08aba22390
KUNAL SHARMA <kunal03b@gmail.com>
2022-11-03 20:36:47 +0530

    updated

--- Experiment 1/ArrayInsertion.cpp
+++ Experiment 1/ArrayInsertion.cpp
@@ -2,24 +2,29 @@
 
 #include <iostream>
 using namespace std;
-void insertelement(int *arr, int element, int &size, int pos){
+void insertelement(int *arr, int element, int &size, int pos)
+{
     size = size + 1;
-    for (int i = size; i >= pos; i--){
+    for (int i = size; i >= pos; i--)
+    {
         arr[i] = arr[i - 1];
     }
     arr[pos - 1] = element;
 }
-int main(){
+int main()
+{
     int arr[10000] = {0};
     cout << "enter the size of array" << endl;
     int size;
     cin >> size;
     cout << "enter the array elements" << endl;
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++)
+    {
         cin >> arr[i];
     }
     cout << "printing the array before the insertion" << endl;
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -31,7 +36,8 @@ int main(){
     cin >> position;
     insertelement(arr, element, size, position);
     cout << "printing the array after the insertion" << endl;
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
     }
 }
--- Experiment 1/Deletion.cpp
+++ Experiment 1/Deletion.cpp
@@ -1,29 +1,40 @@
 // Deletion in an array
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-void deleteelement(int *arr, int &size, int pos){
-    for (int i = pos - 1; i < size - 1; i++){
-        arr[i] = arr[i + 1];}
-    size = size - 1;}
-int main(){
+void deleteelement(int *arr, int &size, int pos)
+{
+    for (int i = pos - 1; i < size - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    size = size - 1;
+}
+
+int main()
+{
     int arr[10000] = {0};
     cout << "enter the size of array" << endl;
     int size;
     cin >> size;
     cout << "enter the array elements" << endl;
-    for (int i = 0; i < size; i++){
-        cin >> arr[i];}
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
     cout << "printing the array before the deletion" << endl;
-    for (int i = 0; i < size; i++){
-        cout << arr[i] << " ";}
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
     cout << endl;
     int position;
     cout << "enter the position" << endl;
     cin >> position;
     deleteelement(arr, size, position);
     cout << "printing the array after the deletion" << endl;
-    for (int i = 0; i < size; i++){
-        cout << arr[i] << " ";}
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
-
